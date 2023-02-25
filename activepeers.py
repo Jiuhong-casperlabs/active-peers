7a42eeb1e527932898e6153f4c517cd22596cc12
Jiuhong <jiuhong@casperlabs.io>
2023-02-25 18:00:30 +0800

    update

--- activepeers.py
+++ activepeers.py
@@ -1,7 +1,6 @@
 import argparse
 import json
-import time
-import multiprocessing
+import threading
 import socket
 from pycspr import NodeClient, NodeConnection
 
@@ -64,16 +63,12 @@ def _main(args: argparse.Namespace):
     active_peers = [x["address"].split(":")[0] for x in node_peers]
 
     # creating process
-    processes_list = [multiprocessing.Process(
+    threads_list = [threading.Thread(
         target=get_rpc_sse_open, args=(peer,)) for peer in active_peers]
 
     # starting process 1 - n
-    for process in processes_list:
-        process.start()
-
-    time.sleep(1)
-    for process in processes_list:
-        process.terminate()
+    for thread in threads_list:
+        thread.start()
 
     print("\nActive peers with rpc and sse ports opened.")
 
